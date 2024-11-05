f145ee982c39ad9f3ba09e8403627a302c102411
WokCao <henryquoccao@gmail.com>
2024-11-05 21:48:11 +0700

    Update: Change method

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
     whitelist: true, // Automatically strip properties that do not have decorators
     forbidNonWhitelisted: true, // Return an error if non-whitelisted properties are present
   }));
-  app.enableCors({ "origin": "https://wokcao.github.io/authen-fe", "methods": "get" })
+  app.enableCors({ "origin": "https://wokcao.github.io/authen-fe", "methods": "*" })
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
